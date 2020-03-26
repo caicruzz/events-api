0ebc440d43ec3243681dfeb6fb2ec9988bbf0252
caicruzz <caicruzz17@gmail.com>
2020-03-26 00:30:44 -0400

    Enabled Cors

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 
 const calendarEventRouter = require('./routes/calendar-event');
+const cors = require('cors');
+const corsOptions = { origin: 'http://localhost:4200'};
 require('./db/mongoose');
 
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(calendarEventRouter);
 
 app.listen(3000, () => {
